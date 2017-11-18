c4e0a69725caae1fa805d49028e2822e13282114
zbz <nnzbz@qq.com>
2017-11-18 15:36:22 +0800

    1.0.3-SNAPSHOT

--- config-server/src/main/java/rebue/scx/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/rebue/scx/config/server/ConfigServerApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//@Configuration
-////@EnableAutoConfiguration
-//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
