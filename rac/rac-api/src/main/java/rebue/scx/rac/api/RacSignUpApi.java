10dd5d751d7702795e315ae7901b14b0a12f74da
zbz <nnzbz@qq.com>
2020-06-28 18:37:46 +0800

    * 补充注释 - 取消自定义hibernate-validator.version，使用spring-boot默认 + 引入Validation校验的功能

--- rac/rac-api/src/main/java/rebue/scx/rac/api/RacSignUpApi.java
+++ rac/rac-api/src/main/java/rebue/scx/rac/api/RacSignUpApi.java
@@ -1,5 +1,7 @@
 package rebue.scx.rac.api;
 
+import javax.validation.Valid;
+
 import org.springframework.validation.annotation.Validated;
 
 import rebue.robotech.ro.Ro;
@@ -9,15 +11,16 @@ import rebue.scx.rac.to.SignUpByUserNameTo;
 /**
  * API用户注册服务
  */
-//在接口上方必须写上 @Validated 注解；
-//参数是POJO类时用 @Validated 注解在参数类型的前面进行修饰；
-//而如果是普通参数，则在方法的上方写上 @Validated 注解，具体约束的注解直接写在参数类型的前面
+// 在接口上方必须写上 @Validated 注解；
+// 有分组时，在方法上方必须写上 @Validated 注解及分组；
+// 参数是POJO类时用 @Valid 注解在参数类型的前面进行修饰；
+// 而如果是普通参数，则在方法的上方写上 @Validated 注解，具体约束的注解直接写在参数类型的前面
 @Validated
 public interface RacSignUpApi {
 
     /**
      * 通过用户名称注册
      */
-    Ro<SignUpRa> signUpByUserName(@Validated SignUpByUserNameTo to);
+    Ro<SignUpRa> signUpByUserName(@Valid SignUpByUserNameTo to);
 
 }
