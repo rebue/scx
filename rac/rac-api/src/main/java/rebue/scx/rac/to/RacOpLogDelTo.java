3c78b357a0d64e439bb0c92f7d662516c7692fd1
zbz <nnzbz@21cn.com>
2020-09-07 07:50:28 +0800

    临时提交

---
+++ rac/rac-api/src/main/java/rebue/scx/rac/to/RacOpLogDelTo.java
@@ -0,0 +1,63 @@
+package rebue.scx.rac.to;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.validation.constraints.PositiveOrZero;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * 用户操作日志
+ *
+ * @mbg.generated 自动生成，如需修改，请删除本行
+ */
+@Data
+@JsonInclude(Include.NON_NULL)
+public class RacOpLogDelTo implements Serializable {
+
+    /**
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 用户ID(如为1则是散客)
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @PositiveOrZero(message = "用户ID不能为负数")
+    private Long userId;
+
+    /**
+     * 系统ID
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @Length(max = 32, message = "系统ID的长度不能大于32")
+    private String sysId;
+
+    /**
+     * 操作标题
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @Length(max = 32, message = "操作标题的长度不能大于32")
+    private String opTitle;
+
+    /**
+     * 操作详情
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    @Length(max = 300, message = "操作详情的长度不能大于300")
+    private String opDetail;
+
+    /**
+     * 操作时间
+     *
+     * @mbg.generated 自动生成，如需修改，请删除本行
+     */
+    private LocalDateTime opDatetime;
+}
