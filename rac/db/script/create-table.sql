a77efa0242c31369378b6c7ec46937a9985407ae
zbz <nnzbz@qq.com>
2021-01-22 11:22:44 +0800

    * 修改模型及数据库 USER改为ACCOUNT * 修改模型及数据库 ORDER_NO改为SEQ_NO

--- rac/db/script/create-table.sql
+++ rac/db/script/create-table.sql
@@ -1,26 +1,44 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2021/1/1 13:31:44                            */
+/* Created on:     2021/1/22 11:05:08                           */
 /*==============================================================*/
 
+
+
+alter table RAC_ACCOUNT 
+   drop foreign key FK_ACCOUNT_AND_PERSON;
+
+alter table RAC_ACCOUNT 
+   drop foreign key FK_ACCOUNT_AND_ORG;
+
+drop table if exists RAC_ACCOUNT;
+
+
+alter table RAC_ACCOUNT_ROLE 
+   drop foreign key FK_ACCOUNT_ROLE_AND_ROLE;
+
+alter table RAC_ACCOUNT_ROLE 
+   drop foreign key FK_ACCOUNT_ROLE_AND_ACCOUNT;
+
+drop table if exists RAC_ACCOUNT_ROLE;
 
 drop table if exists RAC_DOMAIN;
 
 
-alter table RAC_DOMAIN_USER 
-   drop foreign key FK_DOMAIN_USER_AND_DOMAIN;
+alter table RAC_DOMAIN_ACCOUNT 
+   drop foreign key FK_DOMAIN_ACCOUNT_AND_DOMAIN;
 
-alter table RAC_DOMAIN_USER 
-   drop foreign key FK_DOMAIN_USER_AND_USER;
+alter table RAC_DOMAIN_ACCOUNT 
+   drop foreign key FK_DOMAIN_ACCOUNT_AND_ACCOUNT;
 
-drop table if exists RAC_DOMAIN_USER;
+drop table if exists RAC_DOMAIN_ACCOUNT;
 
 
 alter table RAC_LOCK_LOG 
    drop foreign key FK_LOCK_LOG_AND_SYS;
 
 alter table RAC_LOCK_LOG 
-   drop foreign key FK_LOCK_LOG_AND_LOCK_USER;
+   drop foreign key FK_LOCK_LOG_AND_LOCK_ACCOUNT;
 
 alter table RAC_LOCK_LOG 
    drop foreign key FK_LOCK_LOG_AND_LOCK_OP;
@@ -32,7 +50,7 @@ drop table if exists RAC_LOCK_LOG;
 
 
 alter table RAC_OP_LOG 
-   drop foreign key FK_OP_LOG_AND_USER;
+   drop foreign key FK_OP_LOG_AND_ACCOUNT;
 
 alter table RAC_OP_LOG 
    drop foreign key FK_OP_LOG_AND_SYS;
@@ -49,13 +67,13 @@ alter table RAC_ORG
 drop table if exists RAC_ORG;
 
 
-alter table RAC_ORG_USER 
-   drop foreign key FK_ORG_USER_AND_ORG;
+alter table RAC_ORG_ACCOUNT 
+   drop foreign key FK_ORG_ACCOUNT_AND_ORG;
 
-alter table RAC_ORG_USER 
-   drop foreign key FK_ORG_USER_AND_USER;
+alter table RAC_ORG_ACCOUNT 
+   drop foreign key FK_ORG_ACCOUNT_AND_ACCOUNT;
 
-drop table if exists RAC_ORG_USER;
+drop table if exists RAC_ORG_ACCOUNT;
 
 
 alter table RAC_PERM 
@@ -110,23 +128,64 @@ alter table RAC_SYS
 
 drop table if exists RAC_SYS;
 
+/*==============================================================*/
+/* Table: RAC_ACCOUNT                                           */
+/*==============================================================*/
+create table RAC_ACCOUNT
+(
+   ID                   bigint unsigned not null  comment '账户ID',
+   PERSON_ID            bigint unsigned  comment '个人ID',
+   ORG_ID               bigint unsigned  comment '组织ID',
+   IS_ENABLED           bool not null default true  comment '是否启用',
+   SIGN_IN_NAME         varchar(20)  comment '登录名称',
+   SIGN_IN_MOBILE       varchar(11)  comment '登录手机',
+   SIGN_IN_EMAIL        varchar(50)  comment '登录邮箱',
+   SIGN_IN_PSWD         varchar(32)  comment '登录密码(小写(MD5(小写(MD5(密码明文))+小写(密码组合码))))
+             注意：
+             1. 计算方法中的密码在前端传过来时推荐先进行md5序列化，以避免在密码传递过程中使用明码被截获
+             2. 密码组合码在生成密码时随机生成并保存下来，和密码组合起来使用，增加破解的难度',
+   SIGN_IN_PSWD_SALT    char(6)  comment '登录密码组合码(与密码组合加密用，详见登录密码备注)',
+   PAY_PSWD             varchar(32)  comment '支付密码(小写(MD5(小写(MD5(密码明文))+小写(密码组合码))))',
+   PAY_PSWD_SALT        char(6)  comment '支付密码组合码(与支付密码组合加密用，详见支付密码备注)',
+   SIGN_IN_NICKNAME     varchar(20)  comment '登录账户昵称',
+   SIGN_IN_AVATAR       varchar(300)  comment '登录账户头像',
+   WX_OPEN_ID           varchar(64)  comment '微信的OpenId',
+   WX_UNION_ID          varchar(64)  comment '微信的UnionId',
+   WX_NICKNAME          varchar(100)  comment '微信昵称',
+   WX_AVATAR            varchar(300)  comment '微信头像',
+   QQ_OPEN_ID           varchar(64)  comment 'QQ的OpenId',
+   QQ_UNION_ID          varchar(64)  comment 'QQ的UnionId',
+   QQ_NICKNAME          varchar(100)  comment 'QQ昵称',
+   QQ_AVATAR            varchar(300)  comment 'QQ头像',
+   IS_TESTER            bool not null default false  comment '是否测试者',
+   CREATE_TIMESTAMP     bigint unsigned not null  comment '建立时间戳',
+   UPDATE_TIMESTAMP     bigint unsigned not null  comment '修改时间戳',
+   primary key (ID),
+   unique key AK_LOGIN_NICKNAME (SIGN_IN_NICKNAME),
+   unique key AK_LOGIN_NAME (SIGN_IN_NAME),
+   unique key AK_LOGIN_MOBILE (SIGN_IN_MOBILE),
+   unique key AK_LOGIN_EMAIL (SIGN_IN_EMAIL),
+   unique key AK_WX_OPEN_ID (WX_OPEN_ID),
+   unique key AK_WX_UNION_ID (WX_UNION_ID),
+   unique key AK_QQ_OPEN_ID (QQ_OPEN_ID),
+   unique key AK_QQ_UNION_ID (QQ_UNION_ID)
+);
 
-alter table RAC_USER 
-   drop foreign key FK_USER_AND_PERSON;
+alter table RAC_ACCOUNT comment '账户';
 
-alter table RAC_USER 
-   drop foreign key FK_USER_AND_ORG;
+/*==============================================================*/
+/* Table: RAC_ACCOUNT_ROLE                                      */
+/*==============================================================*/
+create table RAC_ACCOUNT_ROLE
+(
+   ID                   bigint unsigned not null  comment '用户角色ID',
+   ROLE_ID              bigint unsigned not null  comment '角色ID',
+   ACCOUNT_ID           bigint unsigned not null  comment '账户ID',
+   primary key (ID),
+   unique key AK_ACCOUNT_AND_ROLE (ROLE_ID, ACCOUNT_ID)
+);
 
-drop table if exists RAC_USER;
-
-
-alter table RAC_USER_ROLE 
-   drop foreign key FK_USER_ROLE_AND_ROLE;
-
-alter table RAC_USER_ROLE 
-   drop foreign key FK_USER_ROLE_AND_USER;
-
-drop table if exists RAC_USER_ROLE;
+alter table RAC_ACCOUNT_ROLE comment '账户角色';
 
 /*==============================================================*/
 /* Table: RAC_DOMAIN                                            */
@@ -143,19 +202,18 @@ create table RAC_DOMAIN
 alter table RAC_DOMAIN comment '领域';
 
 /*==============================================================*/
-/* Table: RAC_DOMAIN_USER                                       */
+/* Table: RAC_DOMAIN_ACCOUNT                                    */
 /*==============================================================*/
-create table RAC_DOMAIN_USER
+create table RAC_DOMAIN_ACCOUNT
 (
    ID                   bigint unsigned not null  comment '领域用户ID',
    DOMAIN_ID            varchar(32) not null  comment '领域ID',
-   USER_ID              bigint unsigned not null  comment '用户ID',
-   IS_ENABLED           bool not null default true  comment '是否启用',
+   ACCOUNT_ID           bigint unsigned not null  comment '账户ID',
    primary key (ID),
-   unique key AK_DOMAIN_AND_USER (DOMAIN_ID, USER_ID)
+   unique key AK_DOMAIN_AND_ACCOUNT (DOMAIN_ID, ACCOUNT_ID)
 );
 
-alter table RAC_DOMAIN_USER comment '领域用户';
+alter table RAC_DOMAIN_ACCOUNT comment '领域账户';
 
 /*==============================================================*/
 /* Table: RAC_LOCK_LOG                                          */
@@ -164,16 +222,16 @@ create table RAC_LOCK_LOG
 (
    ID                   bigint unsigned not null  comment '锁定日志ID',
    SYS_ID               varchar(32) not null  comment '系统ID',
-   LOCK_USER_ID         bigint unsigned not null  comment '锁定用户的用户ID',
-   LOCK_OP_ID           bigint unsigned not null  comment '锁定操作员的用户ID',
+   LOCK_ACCOUNT_ID      bigint unsigned not null  comment '锁定账户的账户ID',
+   LOCK_OP_ID           bigint unsigned not null  comment '锁定操作员的账户ID',
    LOCK_REASON          varchar(100) not null  comment '锁定原因',
    LOCK_DATETIME        datetime not null  comment '锁定时间',
    UNLOCK_REASON        varchar(100) not null  comment '解锁原因',
    UNLOCK_DATETIME      datetime  comment '解锁时间',
-   UNLOCK_OP_ID         bigint unsigned  comment '解锁操作员的用户ID',
+   UNLOCK_OP_ID         bigint unsigned  comment '解锁操作员的账户ID',
    primary key (ID),
-   key AK_USER_AND_LOCK_DATETIME (LOCK_USER_ID, LOCK_DATETIME),
-   key AK_USER_AND_UNLOCK_DATETIME (LOCK_USER_ID, UNLOCK_DATETIME)
+   key AK_ACCOUNT_AND_LOCK_DATETIME (LOCK_ACCOUNT_ID, LOCK_DATETIME),
+   key AK_ACCOUNT_AND_UNLOCK_DATETIME (LOCK_ACCOUNT_ID, UNLOCK_DATETIME)
 );
 
 alter table RAC_LOCK_LOG comment '锁定日志';
@@ -185,13 +243,13 @@ create table RAC_OP_LOG
 (
    ID                   bigint unsigned not null  comment '操作日志ID',
    SYS_ID               varchar(32) not null  comment '系统ID',
-   USER_ID              bigint unsigned not null  comment '用户ID',
+   ACCOUNT_ID           bigint unsigned not null  comment '账户ID',
    OP_TYPE              varchar(20) not null  comment '操作类型',
    OP_TITLE             varchar(32) not null  comment '操作标题',
    OP_DETAIL            varchar(300) not null  comment '操作详情',
    OP_DATETIME          datetime not null  comment '操作时间',
    primary key (ID),
-   key AK_USER_AND_OP_TYPE_AND_DATETIME (USER_ID, OP_TYPE, OP_DATETIME)
+   key AK_ACCOUNT_AND_OP_TYPE_AND_DATETIME (ACCOUNT_ID, OP_TYPE, OP_DATETIME)
 );
 
 alter table RAC_OP_LOG comment '操作日志';
@@ -219,18 +277,18 @@ create table RAC_ORG
 alter table RAC_ORG comment '组织';
 
 /*==============================================================*/
-/* Table: RAC_ORG_USER                                          */
+/* Table: RAC_ORG_ACCOUNT                                       */
 /*==============================================================*/
-create table RAC_ORG_USER
+create table RAC_ORG_ACCOUNT
 (
    ID                   bigint unsigned not null  comment '组织用户ID',
    ORG_ID               bigint unsigned not null  comment '组织ID',
-   USER_ID              bigint unsigned not null  comment '用户ID',
+   ACCOUNT_ID           bigint unsigned not null  comment '账户ID',
    primary key (ID),
-   unique key AK_ORG_AND_USER (ORG_ID, USER_ID)
+   unique key AK_ORG_AND_ACCOUNT (ORG_ID, ACCOUNT_ID)
 );
 
-alter table RAC_ORG_USER comment '组织用户';
+alter table RAC_ORG_ACCOUNT comment '组织账户';
 
 /*==============================================================*/
 /* Table: RAC_PERM                                              */
@@ -242,7 +300,7 @@ create table RAC_PERM
    GROUP_ID             bigint unsigned not null  comment '权限分组ID',
    NAME                 varchar(20) not null  comment '权限名称',
    IS_ENABLED           bool not null default true  comment '是否启用',
-   ORDER_NO             tinyint unsigned not null  comment '顺序号',
+   SEQ_NO               tinyint unsigned not null  comment '顺序号',
    REMARK               varchar(50)  comment '权限备注',
    primary key (ID),
    unique key AK_PERM_GROUP_AND_PERM_NAME (NAME, GROUP_ID)
@@ -259,7 +317,7 @@ create table RAC_PERM_GROUP
    DOMAIN_ID            varchar(32) not null  comment '领域ID',
    NAME                 varchar(20) not null  comment '权限分组名称',
    IS_ENABLED           bool not null default true  comment '是否启用(如果分组没有启用，其下所有权限都要被设置为不启用；只要有一个权限启用，其分组就必须启用)',
-   ORDER_NO             tinyint unsigned not null  comment '顺序号',
+   SEQ_NO               tinyint unsigned not null  comment '顺序号',
    REMARK               varchar(50)  comment '权限分组备注',
    primary key (ID),
    unique key AK_DOMAIN_AND_GROUP_NAME (NAME, DOMAIN_ID)
@@ -302,9 +360,6 @@ alter table RAC_PERM_URN comment '权限URN';
 create table RAC_PERSON
 (
    ID                   bigint unsigned not null  comment '个人ID',
-   IS_ENABLED           bool not null default true  comment '是否启用',
-   PAY_PSWD             varchar(32)  comment '支付密码(小写(MD5(小写(MD5(密码明文))+小写(密码组合码))))',
-   PAY_PSWD_SALT        char(6)  comment '支付密码组合码(与支付密码组合加密用，详见支付密码备注)',
    MOBILE               varchar(11)  comment '手机',
    IS_VERIFIED_MOBILE   bool default false  comment '是否已验证手机号码',
    EMAIL                varchar(50)  comment '电子邮箱',
@@ -333,7 +388,7 @@ create table RAC_ROLE
    NAME                 varchar(20) not null  comment '角色名称',
    DOMAIN_ID            varchar(32) not null  comment '领域ID',
    IS_ENABLED           bool not null default true  comment '是否启用',
-   ORDER_NO             tinyint unsigned not null  comment '顺序号',
+   SEQ_NO               tinyint unsigned not null  comment '顺序号',
    REMARK               varchar(50)  comment '角色备注',
    primary key (ID),
    unique key AK_SYS_AND_ROLE_NAME (NAME)
@@ -372,83 +427,38 @@ create table RAC_SYS
 
 alter table RAC_SYS comment '系统';
 
-/*==============================================================*/
-/* Table: RAC_USER                                              */
-/*==============================================================*/
-create table RAC_USER
-(
-   ID                   bigint unsigned not null  comment '用户ID',
-   PERSON_ID            bigint unsigned  comment '个人ID',
-   ORG_ID               bigint unsigned  comment '组织ID',
-   IS_ENABLED           bool not null default true  comment '是否启用',
-   SIGN_IN_NAME         varchar(20)  comment '登录名称',
-   SIGN_IN_MOBILE       varchar(11)  comment '登录手机',
-   SIGN_IN_EMAIL        varchar(50)  comment '登录邮箱',
-   SIGN_IN_PSWD         varchar(32)  comment '登录密码(小写(MD5(小写(MD5(密码明文))+小写(密码组合码))))
-             注意：
-             1. 计算方法中的密码在前端传过来时推荐先进行md5序列化，以避免在密码传递过程中使用明码被截获
-             2. 密码组合码在生成密码时随机生成并保存下来，和密码组合起来使用，增加破解的难度',
-   SIGN_IN_PSWD_SALT    char(6)  comment '登录密码组合码(与密码组合加密用，详见登录密码备注)',
-   SIGN_IN_NICKNAME     varchar(20)  comment '登录用户昵称',
-   SIGN_IN_AVATAR       varchar(300)  comment '登录用户头像',
-   WX_OPEN_ID           varchar(64)  comment '微信的OpenId',
-   WX_UNION_ID          varchar(64)  comment '微信的UnionId',
-   WX_NICKNAME          varchar(100)  comment '微信昵称',
-   WX_AVATAR            varchar(300)  comment '微信头像',
-   QQ_OPEN_ID           varchar(64)  comment 'QQ的OpenId',
-   QQ_UNION_ID          varchar(64)  comment 'QQ的UnionId',
-   QQ_NICKNAME          varchar(100)  comment 'QQ昵称',
-   QQ_AVATAR            varchar(300)  comment 'QQ头像',
-   IS_TESTER            bool not null default false  comment '是否测试者',
-   CREATE_TIMESTAMP     bigint unsigned not null  comment '建立时间戳',
-   UPDATE_TIMESTAMP     bigint unsigned not null  comment '修改时间戳',
-   primary key (ID),
-   unique key AK_LOGIN_NICKNAME (SIGN_IN_NICKNAME),
-   unique key AK_LOGIN_NAME (SIGN_IN_NAME),
-   unique key AK_LOGIN_MOBILE (SIGN_IN_MOBILE),
-   unique key AK_LOGIN_EMAIL (SIGN_IN_EMAIL),
-   unique key AK_WX_OPEN_ID (WX_OPEN_ID),
-   unique key AK_WX_UNION_ID (WX_UNION_ID),
-   unique key AK_QQ_OPEN_ID (QQ_OPEN_ID),
-   unique key AK_QQ_UNION_ID (QQ_UNION_ID)
-);
+alter table RAC_ACCOUNT add constraint FK_ACCOUNT_AND_PERSON foreign key (PERSON_ID)
+      references RAC_PERSON (ID) on delete restrict on update restrict;
 
-alter table RAC_USER comment '用户';
+alter table RAC_ACCOUNT add constraint FK_ACCOUNT_AND_ORG foreign key (ORG_ID)
+      references RAC_ORG (ID) on delete restrict on update restrict;
 
-/*==============================================================*/
-/* Table: RAC_USER_ROLE                                         */
-/*==============================================================*/
-create table RAC_USER_ROLE
-(
-   ID                   bigint unsigned not null  comment '用户角色ID',
-   ROLE_ID              bigint unsigned not null  comment '角色ID',
-   USER_ID              bigint unsigned not null  comment '用户ID',
-   primary key (ID),
-   unique key AK_USER_AND_ROLE (ROLE_ID, USER_ID)
-);
+alter table RAC_ACCOUNT_ROLE add constraint FK_ACCOUNT_ROLE_AND_ROLE foreign key (ROLE_ID)
+      references RAC_ROLE (ID) on delete restrict on update restrict;
 
-alter table RAC_USER_ROLE comment '用户角色';
+alter table RAC_ACCOUNT_ROLE add constraint FK_ACCOUNT_ROLE_AND_ACCOUNT foreign key (ACCOUNT_ID)
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
-alter table RAC_DOMAIN_USER add constraint FK_DOMAIN_USER_AND_DOMAIN foreign key (DOMAIN_ID)
+alter table RAC_DOMAIN_ACCOUNT add constraint FK_DOMAIN_ACCOUNT_AND_DOMAIN foreign key (DOMAIN_ID)
       references RAC_DOMAIN (ID) on delete restrict on update restrict;
 
-alter table RAC_DOMAIN_USER add constraint FK_DOMAIN_USER_AND_USER foreign key (USER_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
+alter table RAC_DOMAIN_ACCOUNT add constraint FK_DOMAIN_ACCOUNT_AND_ACCOUNT foreign key (ACCOUNT_ID)
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
 alter table RAC_LOCK_LOG add constraint FK_LOCK_LOG_AND_SYS foreign key (SYS_ID)
       references RAC_SYS (ID) on delete restrict on update restrict;
 
-alter table RAC_LOCK_LOG add constraint FK_LOCK_LOG_AND_LOCK_USER foreign key (LOCK_USER_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
+alter table RAC_LOCK_LOG add constraint FK_LOCK_LOG_AND_LOCK_ACCOUNT foreign key (LOCK_ACCOUNT_ID)
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
 alter table RAC_LOCK_LOG add constraint FK_LOCK_LOG_AND_LOCK_OP foreign key (LOCK_OP_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
 alter table RAC_LOCK_LOG add constraint FK_LOCK_LOG_AND_UNLOCK_OP foreign key (UNLOCK_OP_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
-alter table RAC_OP_LOG add constraint FK_OP_LOG_AND_USER foreign key (USER_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
+alter table RAC_OP_LOG add constraint FK_OP_LOG_AND_ACCOUNT foreign key (ACCOUNT_ID)
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
 alter table RAC_OP_LOG add constraint FK_OP_LOG_AND_SYS foreign key (SYS_ID)
       references RAC_SYS (ID) on delete restrict on update restrict;
@@ -459,11 +469,11 @@ alter table RAC_ORG add constraint FK_ORG_AND_ORG foreign key (PARENT_ID)
 alter table RAC_ORG add constraint FK_ORG_AND_DOMAIN foreign key (DOMAIN_ID)
       references RAC_DOMAIN (ID) on delete restrict on update restrict;
 
-alter table RAC_ORG_USER add constraint FK_ORG_USER_AND_ORG foreign key (ORG_ID)
+alter table RAC_ORG_ACCOUNT add constraint FK_ORG_ACCOUNT_AND_ORG foreign key (ORG_ID)
       references RAC_ORG (ID) on delete restrict on update restrict;
 
-alter table RAC_ORG_USER add constraint FK_ORG_USER_AND_USER foreign key (USER_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
+alter table RAC_ORG_ACCOUNT add constraint FK_ORG_ACCOUNT_AND_ACCOUNT foreign key (ACCOUNT_ID)
+      references RAC_ACCOUNT (ID) on delete restrict on update restrict;
 
 alter table RAC_PERM add constraint FK_PERM_AND_PERM_GROUP foreign key (GROUP_ID)
       references RAC_PERM_GROUP (ID) on delete restrict on update restrict;
@@ -494,16 +504,4 @@ alter table RAC_ROLE_PERM add constraint FK_ROLE_PERM_AND_ROLE foreign key (ROLE
 
 alter table RAC_SYS add constraint FK_SYS_AND_DOMAIN foreign key (DOMAIN_ID)
       references RAC_DOMAIN (ID) on delete restrict on update restrict;
-
-alter table RAC_USER add constraint FK_USER_AND_PERSON foreign key (PERSON_ID)
-      references RAC_PERSON (ID) on delete restrict on update restrict;
-
-alter table RAC_USER add constraint FK_USER_AND_ORG foreign key (ORG_ID)
-      references RAC_ORG (ID) on delete restrict on update restrict;
-
-alter table RAC_USER_ROLE add constraint FK_USER_ROLE_AND_ROLE foreign key (ROLE_ID)
-      references RAC_ROLE (ID) on delete restrict on update restrict;
-
-alter table RAC_USER_ROLE add constraint FK_USER_ROLE_AND_USER foreign key (USER_ID)
-      references RAC_USER (ID) on delete restrict on update restrict;
 
