aed4236f46b494586770a8ff062b58c822afb3a1
zbz <nnzbz@qq.com>
2021-06-07 18:36:25 +0800

    * 字典相关表的ID类型改为varchar

--- rac/db/script/create-table.sql
+++ rac/db/script/create-table.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2021/6/7 17:12:25                            */
+/* Created on:     2021/6/7 18:35:16                            */
 /*==============================================================*/
 
 
@@ -244,7 +244,7 @@ alter table RAC_DELEGATION comment '委托';
 /*==============================================================*/
 create table RAC_DIC
 (
-   ID                   bigint unsigned not null  comment '字典ID',
+   ID                   varchar(32) not null  comment '字典ID',
    NAME                 varchar(32) not null  comment '字典名称',
    DOMAIN_ID            varchar(32)  comment '领域ID',
    SYS_ID               varchar(32)  comment '系统ID',
@@ -260,8 +260,8 @@ alter table RAC_DIC comment '字典';
 /*==============================================================*/
 create table RAC_DIC_ITEM
 (
-   ID                   bigint unsigned not null  comment '字典项ID',
-   DIC_ID               bigint unsigned not null  comment '字典ID',
+   ID                   varchar(32) not null  comment '字典项ID',
+   DIC_ID               varchar(32) not null  comment '字典ID',
    NAME                 varchar(32) not null  comment '字典项名称',
    REMARK               varchar(50)  comment '字典备注',
    primary key (ID),
