38e16048afcaf5a053e7a9b5f6ff422eb437c182
yuanman-one <1782271387@qq.com>
2021-06-16 18:24:44 +0800

    *API暴露lintIn方法

--- rac/rac-svr/src/main/java/rebue/scx/rac/api/impl/RacOrgApiImpl.java
+++ rac/rac-svr/src/main/java/rebue/scx/rac/api/impl/RacOrgApiImpl.java
@@ -1,5 +1,7 @@
 package rebue.scx.rac.api.impl;
 
+import java.util.List;
+
 import org.apache.dubbo.config.annotation.DubboService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,99 +30,112 @@ import rebue.scx.rac.to.ex.RacOrgModifyDefaultOrgTo;
  * 组织API实现
  *
  * @mbg.dontOverWriteAnnotation
+ * 
  * @mbg.generated 自动生成的注释，如需修改本注释，请删除本行
  */
 @Slf4j
 @DubboService
 public class RacOrgApiImpl extends BaseApiImpl<java.lang.Long, RacOrgAddTo, RacOrgModifyTo, RacOrgDelTo, RacOrgOneTo, RacOrgListTo, RacOrgPageTo, RacOrgMo, RacOrgJo, RacOrgSvc>
-    implements RacOrgApi {
+		implements RacOrgApi {
 
-    /**
-     * 添加组织账户关系
-     *
-     * @param to 添加的具体信息
-     */
-    @Override
-    public Ro<?> addOrgAccount(RacOrgAccountAddTo to) {
-        _svc.addOrgAccount(to);
-        return new Ro<>(ResultDic.SUCCESS, "添加成功");
-    }
+	/**
+	 * 添加组织账户关系
+	 *
+	 * @param to 添加的具体信息
+	 */
+	@Override
+	public Ro<?> addOrgAccount(final RacOrgAccountAddTo to) {
+		_svc.addOrgAccount(to);
+		return new Ro<>(ResultDic.SUCCESS, "添加成功");
+	}
 
-    /**
-     * 修改账户默认组织的信息
-     *
-     * @param to 修改的具体数据
-     */
-    @Override
-    public Ro<?> modifyDefaultOrg(RacOrgModifyDefaultOrgTo to) {
-        _svc.modifyDefaultOrg(to);
-        return new Ro<>(ResultDic.SUCCESS, "修改成功");
-    }
+	/**
+	 * 修改账户默认组织的信息
+	 *
+	 * @param to 修改的具体数据
+	 */
+	@Override
+	public Ro<?> modifyDefaultOrg(final RacOrgModifyDefaultOrgTo to) {
+		_svc.modifyDefaultOrg(to);
+		return new Ro<>(ResultDic.SUCCESS, "修改成功");
+	}
 
-    /**
-     * 更改组织与账户的关系
-     *
-     * @param to 修改的具体数据
-     */
-    @Override
-    public Ro<?> modifyOrgAccount(RacModifyOrgAccountTo to) {
-        _svc.modifyOrgAccount(to);
-        return new Ro<>(ResultDic.SUCCESS, "更改成功");
-    }
+	/**
+	 * 更改组织与账户的关系
+	 *
+	 * @param to 修改的具体数据
+	 */
+	@Override
+	public Ro<?> modifyOrgAccount(final RacModifyOrgAccountTo to) {
+		_svc.modifyOrgAccount(to);
+		return new Ro<>(ResultDic.SUCCESS, "更改成功");
+	}
 
-    /**
-     * 删除组织账户关系
-     *
-     * @param to 添加的具体信息
-     */
-    @Override
-    public Ro<?> delOrgAccount(RacOrgAccountDelTo to) {
-        _svc.delOrgAccount(to);
-        return new Ro<>(ResultDic.SUCCESS, "移除成功");
-    }
+	/**
+	 * 删除组织账户关系
+	 *
+	 * @param to 添加的具体信息
+	 */
+	@Override
+	public Ro<?> delOrgAccount(final RacOrgAccountDelTo to) {
+		_svc.delOrgAccount(to);
+		return new Ro<>(ResultDic.SUCCESS, "移除成功");
+	}
 
-    /**
-     * 查询组织的信息
-     *
-     * @param qo 查询的具体条件
-     */
-    @Override
-    public Ro<ListRa<RacOrgMo>> list(RacOrgListTo qo) {
-        return new Ro<>(ResultDic.SUCCESS, "查询成功", new ListRa<>(_svc.list(qo)));
-    }
+	/**
+	 * 查询组织的信息
+	 *
+	 * @param qo 查询的具体条件
+	 */
+	@Override
+	public Ro<ListRa<RacOrgMo>> list(final RacOrgListTo qo) {
+		return new Ro<>(ResultDic.SUCCESS, "查询成功", new ListRa<>(_svc.list(qo)));
+	}
 
-    /**
-     * 查询当前账户所在的组织的信息
-     *
-     * @param qo 查询的具体条件
-     */
-    @Override
-    public Ro<ListRa<RacOrgMo>> listByAccountId(RacOrgListByAccountIdTo qo) {
-        return new Ro<>(ResultDic.SUCCESS, "查询成功", new ListRa<>(_svc.listByAccountId(qo)));
-    }
+	/**
+	 * 查询当前账户所在的组织的信息
+	 *
+	 * @param qo 查询的具体条件
+	 */
+	@Override
+	public Ro<ListRa<RacOrgMo>> listByAccountId(final RacOrgListByAccountIdTo qo) {
+		return new Ro<>(ResultDic.SUCCESS, "查询成功", new ListRa<>(_svc.listByAccountId(qo)));
+	}
 
-    /**
-     * 查询可以添加的组织信息
-     *
-     * @param qo 查询的具体条件
-     */
-    @Override
-    public Ro<PageRa<RacOrgMo>> listAddableOrg(RacOrgListByAccountIdTo qo) {
-        return new Ro<>(ResultDic.SUCCESS, "分页查询成功", new PageRa<>(_svc.listAddableOrg(qo)));
-    }
+	/**
+	 * 查询可以添加的组织信息
+	 *
+	 * @param qo 查询的具体条件
+	 */
+	@Override
+	public Ro<PageRa<RacOrgMo>> listAddableOrg(final RacOrgListByAccountIdTo qo) {
+		return new Ro<>(ResultDic.SUCCESS, "分页查询成功", new PageRa<>(_svc.listAddableOrg(qo)));
+	}
 
-    @Override
-    public Ro<PageRa<RacOrgMo>> page(RacOrgPageTo qo) {
-        if (qo.getDeep()) {
-            if (qo.getPageSize() != null && qo.getPageSize() > _limitPageSize) {
-                final String msg = "pageSize不能大于" + _limitPageSize;
-                log.error(msg);
-                throw new IllegalArgumentException(msg);
-            }
-            return new Ro<>(ResultDic.SUCCESS, "分页查询成功", new PageRa<>(_svc.pageIsTable(qo)));
-        }
-        else {
-            return super.page(qo);
-        }
-    }
+	@Override
+	public Ro<PageRa<RacOrgMo>> page(final RacOrgPageTo qo) {
+		if (qo.getDeep()) {
+			if (qo.getPageSize() != null && qo.getPageSize() > _limitPageSize) {
+				final String msg = "pageSize不能大于" + _limitPageSize;
+				log.error(msg);
+				throw new IllegalArgumentException(msg);
+			}
+			return new Ro<>(ResultDic.SUCCESS, "分页查询成功", new PageRa<>(_svc.pageIsTable(qo)));
+		}
+		else {
+			return super.page(qo);
+		}
+	}
+
+	/**
+	 * 根据ID列表查询组织
+	 * 
+	 * @param ids ID列表
+	 * 
+	 */
+	@Override
+	public Ro<ListRa<RacOrgMo>> listIn(final List<Long> ids) {
+		return new Ro<>(ResultDic.SUCCESS, "查询成功", new ListRa<>(_svc.listIn(ids)));
+	}
+
 }
