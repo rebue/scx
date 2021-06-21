85c96c6b50854bcb552c4a15af2055692ffac616
nnzbz <nnzbz@qq.com>
2021-06-21 11:25:27 +0800

    * 补充提交

---
+++ wxx/wxx-gen/src/main/java/rebue/wxx/gen/WxxGen.java
@@ -0,0 +1,19 @@
+package rebue.wxx.gen;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+
+import rebue.mbgx.MybatisGeneratorWrap;
+
+/**
+ * 自动生成Mybatis
+ */
+public class WxxGen {
+
+    public static void main(final String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        MybatisGeneratorWrap.gen(true, "conf/mbg-wxx.properties");
+    }
+}
