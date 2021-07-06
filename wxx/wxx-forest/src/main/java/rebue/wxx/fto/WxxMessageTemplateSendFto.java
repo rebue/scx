7c23715ead98bccd860e78bfb0183d94264ea424
nnzbz <nnzbz@qq.com>
2021-07-06 10:26:39 +0800

    * 补充注释

--- wxx/wxx-forest/src/main/java/rebue/wxx/fto/WxxMessageTemplateSendFto.java
+++ wxx/wxx-forest/src/main/java/rebue/wxx/fto/WxxMessageTemplateSendFto.java
@@ -34,6 +34,34 @@ public class WxxMessageTemplateSendFto {
     private Miniprogram            miniprogram;
     /**
      * 模板数据
+     *
+     * 示例:
+     *
+     * <pre>
+     *  {
+     *      "first": {
+     *          "value":"恭喜你购买成功！",
+     *          "color":"#173177"
+     *      },
+     *      "keyword1":{
+     *          "value":"巧克力",
+     *          "color":"#173177"
+     *      },
+     *      "keyword2": {
+     *          "value":"39.8元",
+     *          "color":"#173177"
+     *      },
+     *      "keyword3": {
+     *          "value":"2014年9月22日",
+     *          "color":"#173177"
+     *      },
+     *      "remark":{
+     *          "value":"欢迎再次购买！",
+     *          "color":"#173177"
+     *      }
+     *  }
+     * </pre>
+     *
      */
     private Map<String, ParamItem> data;
 }
