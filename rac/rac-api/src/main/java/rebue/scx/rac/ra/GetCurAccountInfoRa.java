bfb6c4061bf73fa47e01adecf166998ed6147728
yuanman-one <1782271387@qq.com>
2021-07-12 16:16:53 +0800

    *临时更新

--- rac/rac-api/src/main/java/rebue/scx/rac/ra/GetCurAccountInfoRa.java
+++ rac/rac-api/src/main/java/rebue/scx/rac/ra/GetCurAccountInfoRa.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import rebue.scx.rac.mo.RacPermCommandMo;
 
 /**
  * 当前账户信息
@@ -19,56 +20,60 @@ import lombok.NoArgsConstructor;
 @JsonInclude(Include.NON_NULL)
 public class GetCurAccountInfoRa implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long      serialVersionUID = 1L;
 
-    /**
-     * 账户ID
-     */
-    private Long              id;
+	/**
+	 * 账户ID
+	 */
+	private Long                   id;
 
-    /**
-     * 账户昵称
-     */
-    private String            nickname;
+	/**
+	 * 账户昵称
+	 */
+	private String                 nickname;
 
-    /**
-     * 账户头像
-     */
-    private String            avatar;
+	/**
+	 * 账户头像
+	 */
+	private String                 avatar;
 
-    /**
-     * 代理账户ID
-     */
-    private Long              agentAccountId;
+	/**
+	 * 代理账户ID
+	 */
+	private Long                   agentAccountId;
 
-    /**
-     * 代理账户昵称
-     */
-    private String            agentNickname;
+	/**
+	 * 代理账户昵称
+	 */
+	private String                 agentNickname;
 
-    /**
-     * 代理账户头像
-     */
-    private String            agentAvatar;
+	/**
+	 * 代理账户头像
+	 */
+	private String                 agentAvatar;
 
-    /**
-     * 账户组织ID
-     */
-    private Long              orgId;
+	/**
+	 * 账户组织ID
+	 */
+	private Long                   orgId;
 
-    /**
-     * 账户组织全名
-     */
-    private String            orgFullName;
+	/**
+	 * 账户组织全名
+	 */
+	private String                 orgFullName;
 
-    /**
-     * 是否测试者
-     */
-    private Boolean           isTester;
+	/**
+	 * 是否测试者
+	 */
+	private Boolean                isTester;
 
-    /**
-     * 菜单列表
-     */
-    private List<String>      menus;
+	/**
+	 * 菜单列表
+	 */
+	private List<String>           menus;
+	/**
+	 * 权限命令
+	 */
+	private List<RacPermCommandMo> permCommands;
 
 }
