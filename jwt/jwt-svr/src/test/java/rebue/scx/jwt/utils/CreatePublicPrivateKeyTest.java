48e547ae5890a685a5284df1dae075364cf901df
lq <1213811243@qq.com>
2021-08-25 10:41:17 +0800

    修改jwt库

---
+++ jwt/jwt-svr/src/test/java/rebue/scx/jwt/utils/CreatePublicPrivateKeyTest.java
@@ -0,0 +1,32 @@
+package rebue.scx.jwt.utils;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.security.KeyPair;
+import java.security.Security;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
+
+public class CreatePublicPrivateKeyTest {
+
+    private static final String RSA_KEY_FILE_PATH = "jwt-dev-key-pair.pem";
+
+    private static String publicKeyBase64(RSAPublicKey publicKey)
+    {
+        return Base64.getEncoder().encodeToString(publicKey.getEncoded());
+    }
+
+    @Test
+    private void test() throws Exception
+    {
+        Security.addProvider(new BouncyCastleProvider());
+        try (InputStream in = ResourcesWrapper.getInputStream(RSA_KEY_FILE_PATH)) {
+            KeyPair keyPair = CreatePublicPrivateKey.parseRsaKeyPair(in);
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            System.out.println(publicKeyBase64(publicKey));
+        }
+    }
+
+}
