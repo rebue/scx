b649d10ab141cb65feaf7e2a8c142f6d32ab9642
lq <1213811243@qq.com>
2021-08-26 11:49:21 +0800

    建表语句

---
+++ oap/db/script/create-table.sql
@@ -0,0 +1,64 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80019
+ Source Host           : localhost:3306
+ Source Schema         : oap
+
+ Target Server Type    : MySQL
+ Target Server Version : 80019
+ File Encoding         : 65001
+
+ Date: 26/08/2021 11:48:58
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for OAP_APP
+-- ----------------------------
+DROP TABLE IF EXISTS `OAP_APP`;
+CREATE TABLE `OAP_APP`  (
+  `ID` bigint UNSIGNED NOT NULL COMMENT '主键',
+  `APP_ID` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'rac_app主键',
+  `CLIENT_ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'oidc client id',
+  `SECRET` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'oidc secret',
+  `CREATE_TIMESTAMP` bigint UNSIGNED NOT NULL COMMENT '建立时间戳',
+  `UPDATE_TIMESTAMP` bigint UNSIGNED NOT NULL COMMENT '修改时间戳',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for OAP_IP_WHITE_LIST
+-- ----------------------------
+DROP TABLE IF EXISTS `OAP_IP_WHITE_LIST`;
+CREATE TABLE `OAP_IP_WHITE_LIST`  (
+  `ID` bigint UNSIGNED NOT NULL COMMENT '主键',
+  `APP_ID` bigint UNSIGNED NOT NULL COMMENT 'OAP_APP主键',
+  `IP_ADDR` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '白名单IP',
+  `CREATE_TIMESTAMP` bigint UNSIGNED NOT NULL COMMENT '建立时间戳',
+  `UPDATE_TIMESTAMP` bigint UNSIGNED NOT NULL COMMENT '修改时间戳',
+  PRIMARY KEY (`ID`) USING BTREE,
+  INDEX `FK_IP_WHITE_LIST_APP_ID_REF_APP_ID`(`APP_ID`) USING BTREE,
+  CONSTRAINT `FK_IP_WHITE_LIST_APP_ID_REF_APP_ID` FOREIGN KEY (`APP_ID`) REFERENCES `OAP_APP` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for OAP_REDIRECT_URI
+-- ----------------------------
+DROP TABLE IF EXISTS `OAP_REDIRECT_URI`;
+CREATE TABLE `OAP_REDIRECT_URI`  (
+  `ID` bigint UNSIGNED NOT NULL COMMENT '主键',
+  `APP_ID` bigint UNSIGNED NOT NULL COMMENT 'OAP_APP主键',
+  `REDIRECT_URI` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '允许的重定向URI, 最后一个字符可以是通配符*',
+  `CREATE_TIMESTAMP` bigint UNSIGNED NOT NULL COMMENT '建立时间戳',
+  `UPDATE_TIMESTAMP` bigint UNSIGNED NOT NULL COMMENT '修改时间戳',
+  PRIMARY KEY (`ID`) USING BTREE,
+  INDEX `FK_REDIRECT_URI_APP_ID_REF_APP_ID`(`APP_ID`) USING BTREE,
+  CONSTRAINT `FK_REDIRECT_URI_APP_ID_REF_APP_ID` FOREIGN KEY (`APP_ID`) REFERENCES `OAP_APP` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
