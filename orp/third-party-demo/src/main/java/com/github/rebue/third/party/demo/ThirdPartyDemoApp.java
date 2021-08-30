92328faa9358856bd523ced3cb75546e8f5240dc
lq <1213811243@qq.com>
2021-08-30 14:36:14 +0800

    第三方示例应用

---
+++ orp/third-party-demo/src/main/java/com/github/rebue/third/party/demo/ThirdPartyDemoApp.java
@@ -0,0 +1,14 @@
+package com.github.rebue.third.party.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyDemoApp {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ThirdPartyDemoApp.class, args);
+    }
+
+}
