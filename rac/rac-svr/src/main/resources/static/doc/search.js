5f9c16da928236ef0099ec98bd802ef8ee4d0b81
yuanman <yuanman@yuanman-PC>
2021-08-30 17:36:10 +0800

    *自动生成文档

--- rac/rac-svr/src/main/resources/static/doc/search.js
+++ rac/rac-svr/src/main/resources/static/doc/search.js
@@ -1,34 +1,54 @@
 let api = [];
 api.push({
-    alias: 'RacDicItemCtrl',
+    alias: 'RacPermCtrl',
     order: '1',
-    link: '字典项',
-    desc: '字典项',
+    link: '权限',
+    desc: '权限',
     list: []
 })
 api[0].list.push({
     order: '1',
-    desc: '添加字典项',
+    desc: '添加权限',
 });
 api[0].list.push({
     order: '2',
-    desc: '修改字典项的信息',
+    desc: '修改权限的信息',
 });
 api[0].list.push({
     order: '3',
-    desc: '查询字典项的信息',
+    desc: '删除权限',
 });
 api[0].list.push({
     order: '4',
-    desc: '删除字典项',
+    desc: '获取单个权限的信息',
 });
 api[0].list.push({
     order: '5',
-    desc: '获取单个字典项的信息',
+    desc: '判断权限是否存在',
 });
 api[0].list.push({
     order: '6',
-    desc: '判断字典项是否存在',
+    desc: '查询权限的信息',
+});
+api[0].list.push({
+    order: '7',
+    desc: '查询带分组的权限列表',
+});
+api[0].list.push({
+    order: '8',
+    desc: '上移动权限的信息',
+});
+api[0].list.push({
+    order: '9',
+    desc: '下移动权限的信息',
+});
+api[0].list.push({
+    order: '10',
+    desc: '启用权限',
+});
+api[0].list.push({
+    order: '11',
+    desc: '禁用权限',
 });
 api.push({
     alias: 'RacOrgCtrl',
@@ -90,563 +110,543 @@ api[1].list.push({
     desc: '查询组织的信息',
 });
 api.push({
-    alias: 'RacUserAccountCtrl',
+    alias: 'RacAppCtrl',
     order: '3',
-    link: '带用户信息的账户',
-    desc: '带用户信息的账户',
+    link: '应用',
+    desc: '应用',
     list: []
 })
 api[2].list.push({
     order: '1',
-    desc: '分页查询带有用户信息的账户',
+    desc: '添加应用',
 });
 api[2].list.push({
     order: '2',
-    desc: '根据ID查询有用户信息的账户',
+    desc: '修改应用的信息',
 });
-api.push({
-    alias: 'RacSignInCtrl',
-    order: '4',
-    link: '账户登录的控制器',
-    desc: '账户登录的控制器',
-    list: []
-})
-api[3].list.push({
-    order: '1',
-    desc: '通过账户名称登录',
-});
-api.push({
-    alias: 'RacAgentSignInCtrl',
-    order: '5',
-    link: '代理登录的控制器',
-    desc: '代理登录的控制器',
-    list: []
-})
-api[4].list.push({
-    order: '1',
-    desc: '代理登录',
-});
-api.push({
-    alias: 'RacAgentSignOutCtrl',
-    order: '6',
-    link: '退出代理登录的控制器',
-    desc: '退出代理登录的控制器',
-    list: []
-})
-api[5].list.push({
-    order: '1',
-    desc: '退出代理登录',
-});
-api.push({
-    alias: 'RacSignUpCtrl',
-    order: '7',
-    link: 'api账户注册的控制器',
-    desc: 'API账户注册的控制器',
-    list: []
-})
-api[6].list.push({
-    order: '1',
-    desc: '通过账户名称注册',
-});
-api.push({
-    alias: 'RacUserCtrl',
-    order: '8',
-    link: '用户',
-    desc: '用户',
-    list: []
-})
-api[7].list.push({
-    order: '1',
-    desc: '添加用户',
-});
-api[7].list.push({
-    order: '2',
-    desc: '修改用户的信息',
-});
-api[7].list.push({
+api[2].list.push({
     order: '3',
-    desc: '删除用户',
+    desc: '删除应用',
 });
-api[7].list.push({
+api[2].list.push({
     order: '4',
-    desc: '获取单个用户的信息',
+    desc: '获取单个应用的信息',
 });
-api[7].list.push({
+api[2].list.push({
     order: '5',
-    desc: '判断用户是否存在',
+    desc: '判断应用是否存在',
 });
-api[7].list.push({
+api[2].list.push({
     order: '6',
-    desc: '查询用户的信息',
+    desc: '查询应用的信息',
 });
-api.push({
-    alias: 'RacSysCtrl',
-    order: '9',
-    link: '系统',
-    desc: '系统',
-    list: []
-})
-api[8].list.push({
-    order: '1',
-    desc: '添加系统',
-});
-api[8].list.push({
-    order: '2',
-    desc: '修改系统的信息',
-});
-api[8].list.push({
-    order: '3',
-    desc: '删除系统',
-});
-api[8].list.push({
-    order: '4',
-    desc: '获取单个系统的信息',
-});
-api[8].list.push({
-    order: '5',
-    desc: '判断系统是否存在',
-});
-api[8].list.push({
-    order: '6',
-    desc: '查询系统的信息',
-});
-api[8].list.push({
+api[2].list.push({
     order: '7',
-    desc: '查询系统的信息',
-});
-api.push({
-    alias: 'RacAccountCtrl',
-    order: '10',
-    link: '账户',
-    desc: '账户',
-    list: []
-})
-api[9].list.push({
-    order: '1',
-    desc: '添加账户',
-});
-api[9].list.push({
-    order: '2',
-    desc: '修改账户的信息',
-});
-api[9].list.push({
-    order: '3',
-    desc: '删除账户',
-});
-api[9].list.push({
-    order: '4',
-    desc: '获取单个账户的信息',
-});
-api[9].list.push({
-    order: '5',
-    desc: '判断账户是否存在',
-});
-api[9].list.push({
-    order: '6',
-    desc: '查询账户的信息',
-});
-api[9].list.push({
-    order: '7',
-    desc: '修改账户登录密码',
-});
-api[9].list.push({
-    order: '8',
-    desc: '启用账户',
-});
-api[9].list.push({
-    order: '9',
-    desc: '禁用账户',
-});
-api[9].list.push({
-    order: '10',
-    desc: '上传头像',
-});
-api[9].list.push({
-    order: '11',
-    desc: '获取当前账户信息',
-});
-api[9].list.push({
-    order: '12',
-    desc: '查询账户的信息',
-});
-api.push({
-    alias: 'RacPermMenuCtrl',
-    order: '11',
-    link: '权限菜单',
-    desc: '权限菜单',
-    list: []
-})
-api[10].list.push({
-    order: '1',
-    desc: '添加权限菜单',
-});
-api[10].list.push({
-    order: '2',
-    desc: '添加/修改权限菜单',
-});
-api[10].list.push({
-    order: '3',
-    desc: '修改权限菜单的信息',
-});
-api[10].list.push({
-    order: '4',
-    desc: '删除权限菜单',
-});
-api[10].list.push({
-    order: '5',
-    desc: '获取单个权限菜单的信息',
-});
-api[10].list.push({
-    order: '6',
-    desc: '判断权限菜单是否存在',
-});
-api[10].list.push({
-    order: '7',
-    desc: '查询权限菜单的信息',
-});
-api[10].list.push({
-    order: '8',
-    desc: '查询权限菜单的信息',
+    desc: '查询应用的信息',
 });
 api.push({
     alias: 'RacOpLogCtrl',
-    order: '12',
+    order: '4',
     link: '操作日志',
     desc: '操作日志',
     list: []
 })
-api[11].list.push({
+api[3].list.push({
     order: '1',
     desc: '添加操作日志',
 });
-api[11].list.push({
+api[3].list.push({
     order: '2',
     desc: '修改操作日志的信息',
 });
-api[11].list.push({
+api[3].list.push({
     order: '3',
     desc: '删除操作日志',
 });
-api[11].list.push({
+api[3].list.push({
     order: '4',
     desc: '获取单个操作日志的信息',
 });
-api[11].list.push({
+api[3].list.push({
     order: '5',
     desc: '判断操作日志是否存在',
 });
-api[11].list.push({
+api[3].list.push({
     order: '6',
     desc: '查询操作日志的信息',
 });
 api.push({
-    alias: 'RacLockLogCtrl',
-    order: '13',
-    link: '锁定日志',
-    desc: '锁定日志',
-    list: []
-})
-api[12].list.push({
-    order: '1',
-    desc: '删除锁定日志',
-});
-api[12].list.push({
-    order: '2',
-    desc: '获取单个锁定日志的信息',
-});
-api[12].list.push({
-    order: '3',
-    desc: '判断锁定日志是否存在',
-});
-api[12].list.push({
-    order: '4',
-    desc: '查询锁定日志的信息',
-});
-api[12].list.push({
-    order: '5',
-    desc: '添加锁定日志',
-});
-api[12].list.push({
-    order: '6',
-    desc: '修改锁定日志的信息',
-});
-api.push({
-    alias: 'RacPermGroupCtrl',
-    order: '14',
-    link: '权限分组',
-    desc: '权限分组',
-    list: []
-})
-api[13].list.push({
-    order: '1',
-    desc: '添加权限分组',
-});
-api[13].list.push({
-    order: '2',
-    desc: '修改权限分组的信息',
-});
-api[13].list.push({
-    order: '3',
-    desc: '删除权限分组',
-});
-api[13].list.push({
-    order: '4',
-    desc: '获取单个权限分组的信息',
-});
-api[13].list.push({
-    order: '5',
-    desc: '判断权限分组是否存在',
-});
-api[13].list.push({
-    order: '6',
-    desc: '查询权限分组的信息',
-});
-api[13].list.push({
-    order: '7',
-    desc: '上移动权限分组的信息',
-});
-api[13].list.push({
-    order: '8',
-    desc: '下移动权限分组的信息',
-});
-api[13].list.push({
-    order: '9',
-    desc: '启用权限分组',
-});
-api[13].list.push({
-    order: '10',
-    desc: '禁用权限分组',
-});
-api.push({
     alias: 'RacDicCtrl',
-    order: '15',
+    order: '5',
     link: '字典',
     desc: '字典',
     list: []
 })
-api[14].list.push({
+api[4].list.push({
     order: '1',
     desc: '添加字典',
 });
-api[14].list.push({
+api[4].list.push({
     order: '2',
     desc: '修改字典的信息',
 });
-api[14].list.push({
+api[4].list.push({
     order: '3',
     desc: '查询字典的信息',
 });
-api[14].list.push({
+api[4].list.push({
     order: '4',
     desc: '查询字典的信息',
 });
-api[14].list.push({
+api[4].list.push({
     order: '5',
     desc: '删除字典',
 });
-api[14].list.push({
+api[4].list.push({
     order: '6',
     desc: '获取单个字典的信息',
 });
-api[14].list.push({
+api[4].list.push({
     order: '7',
     desc: '判断字典是否存在',
 });
 api.push({
-    alias: 'RacRoleCtrl',
-    order: '16',
-    link: '角色',
-    desc: '角色',
+    alias: 'RacUserCtrl',
+    order: '6',
+    link: '用户',
+    desc: '用户',
     list: []
 })
-api[15].list.push({
+api[5].list.push({
     order: '1',
-    desc: '添加角色',
+    desc: '添加用户',
 });
-api[15].list.push({
+api[5].list.push({
     order: '2',
-    desc: '添加/修改角色和权限的关系',
+    desc: '修改用户的信息',
 });
-api[15].list.push({
+api[5].list.push({
     order: '3',
-    desc: '添加角色和账户的关系',
+    desc: '删除用户',
 });
-api[15].list.push({
+api[5].list.push({
     order: '4',
-    desc: '删除角色和账户的关系',
+    desc: '获取单个用户的信息',
 });
-api[15].list.push({
+api[5].list.push({
     order: '5',
-    desc: '修改角色的信息',
+    desc: '判断用户是否存在',
 });
-api[15].list.push({
+api[5].list.push({
     order: '6',
-    desc: '删除角色',
-});
-api[15].list.push({
-    order: '7',
-    desc: '获取单个角色的信息',
-});
-api[15].list.push({
-    order: '8',
-    desc: '判断角色是否存在',
-});
-api[15].list.push({
-    order: '9',
-    desc: '查询角色的信息',
-});
-api[15].list.push({
-    order: '10',
-    desc: '上移动角色的信息',
-});
-api[15].list.push({
-    order: '11',
-    desc: '下移动角色的信息',
-});
-api[15].list.push({
-    order: '12',
-    desc: '启用角色',
-});
-api[15].list.push({
-    order: '13',
-    desc: '禁用角色',
-});
-api[15].list.push({
-    order: '14',
-    desc: '查询角色已有的权限的关系',
-});
-api[15].list.push({
-    order: '15',
-    desc: '查询角色的信息',
-});
-api[15].list.push({
-    order: '16',
-    desc: '查询角色的信息',
+    desc: '查询用户的信息',
 });
 api.push({
     alias: 'RacPermCommandCtrl',
-    order: '17',
+    order: '7',
     link: '权限命令',
     desc: '权限命令',
     list: []
 })
-api[16].list.push({
+api[6].list.push({
     order: '1',
     desc: '添加权限命令',
 });
-api[16].list.push({
+api[6].list.push({
     order: '2',
     desc: '修改权限命令的信息',
 });
-api[16].list.push({
+api[6].list.push({
     order: '3',
     desc: '删除权限命令',
 });
-api[16].list.push({
+api[6].list.push({
     order: '4',
     desc: '获取单个权限命令的信息',
 });
-api[16].list.push({
+api[6].list.push({
     order: '5',
     desc: '判断权限命令是否存在',
 });
-api[16].list.push({
+api[6].list.push({
     order: '6',
     desc: '查询权限命令的信息',
 });
-api[16].list.push({
+api[6].list.push({
     order: '7',
     desc: '查询权限命令的信息',
 });
 api.push({
+    alias: 'RacPermGroupCtrl',
+    order: '8',
+    link: '权限分组',
+    desc: '权限分组',
+    list: []
+})
+api[7].list.push({
+    order: '1',
+    desc: '添加权限分组',
+});
+api[7].list.push({
+    order: '2',
+    desc: '修改权限分组的信息',
+});
+api[7].list.push({
+    order: '3',
+    desc: '删除权限分组',
+});
+api[7].list.push({
+    order: '4',
+    desc: '获取单个权限分组的信息',
+});
+api[7].list.push({
+    order: '5',
+    desc: '判断权限分组是否存在',
+});
+api[7].list.push({
+    order: '6',
+    desc: '查询权限分组的信息',
+});
+api[7].list.push({
+    order: '7',
+    desc: '上移动权限分组的信息',
+});
+api[7].list.push({
+    order: '8',
+    desc: '下移动权限分组的信息',
+});
+api[7].list.push({
+    order: '9',
+    desc: '启用权限分组',
+});
+api[7].list.push({
+    order: '10',
+    desc: '禁用权限分组',
+});
+api.push({
+    alias: 'RacRoleCtrl',
+    order: '9',
+    link: '角色',
+    desc: '角色',
+    list: []
+})
+api[8].list.push({
+    order: '1',
+    desc: '添加角色',
+});
+api[8].list.push({
+    order: '2',
+    desc: '添加/修改角色和权限的关系',
+});
+api[8].list.push({
+    order: '3',
+    desc: '添加角色和账户的关系',
+});
+api[8].list.push({
+    order: '4',
+    desc: '删除角色和账户的关系',
+});
+api[8].list.push({
+    order: '5',
+    desc: '修改角色的信息',
+});
+api[8].list.push({
+    order: '6',
+    desc: '删除角色',
+});
+api[8].list.push({
+    order: '7',
+    desc: '获取单个角色的信息',
+});
+api[8].list.push({
+    order: '8',
+    desc: '判断角色是否存在',
+});
+api[8].list.push({
+    order: '9',
+    desc: '查询角色的信息',
+});
+api[8].list.push({
+    order: '10',
+    desc: '上移动角色的信息',
+});
+api[8].list.push({
+    order: '11',
+    desc: '下移动角色的信息',
+});
+api[8].list.push({
+    order: '12',
+    desc: '启用角色',
+});
+api[8].list.push({
+    order: '13',
+    desc: '禁用角色',
+});
+api[8].list.push({
+    order: '14',
+    desc: '查询角色已有的权限的关系',
+});
+api[8].list.push({
+    order: '15',
+    desc: '查询角色的信息',
+});
+api[8].list.push({
+    order: '16',
+    desc: '查询角色的信息',
+});
+api.push({
+    alias: 'RacLockLogCtrl',
+    order: '10',
+    link: '锁定日志',
+    desc: '锁定日志',
+    list: []
+})
+api[9].list.push({
+    order: '1',
+    desc: '删除锁定日志',
+});
+api[9].list.push({
+    order: '2',
+    desc: '获取单个锁定日志的信息',
+});
+api[9].list.push({
+    order: '3',
+    desc: '判断锁定日志是否存在',
+});
+api[9].list.push({
+    order: '4',
+    desc: '查询锁定日志的信息',
+});
+api[9].list.push({
+    order: '5',
+    desc: '添加锁定日志',
+});
+api[9].list.push({
+    order: '6',
+    desc: '修改锁定日志的信息',
+});
+api.push({
     alias: 'RacPermUrnCtrl',
-    order: '18',
+    order: '11',
     link: '权限urn',
     desc: '权限URN',
     list: []
 })
-api[17].list.push({
+api[10].list.push({
     order: '1',
     desc: '添加权限URN',
 });
-api[17].list.push({
+api[10].list.push({
     order: '2',
     desc: '添加修改URN',
 });
-api[17].list.push({
+api[10].list.push({
     order: '3',
     desc: '修改权限URN的信息',
 });
-api[17].list.push({
+api[10].list.push({
     order: '4',
     desc: '删除权限URN',
 });
-api[17].list.push({
+api[10].list.push({
     order: '5',
     desc: '获取单个权限URN的信息',
 });
-api[17].list.push({
+api[10].list.push({
     order: '6',
     desc: '判断权限URN是否存在',
 });
-api[17].list.push({
+api[10].list.push({
     order: '7',
     desc: '查询权限URN的信息',
 });
-api[17].list.push({
+api[10].list.push({
     order: '8',
     desc: '通过permId查询权限URN的信息',
 });
 api.push({
-    alias: 'RacPermCtrl',
+    alias: 'RacDicItemCtrl',
+    order: '12',
+    link: '字典项',
+    desc: '字典项',
+    list: []
+})
+api[11].list.push({
+    order: '1',
+    desc: '添加字典项',
+});
+api[11].list.push({
+    order: '2',
+    desc: '修改字典项的信息',
+});
+api[11].list.push({
+    order: '3',
+    desc: '查询字典项的信息',
+});
+api[11].list.push({
+    order: '4',
+    desc: '删除字典项',
+});
+api[11].list.push({
+    order: '5',
+    desc: '获取单个字典项的信息',
+});
+api[11].list.push({
+    order: '6',
+    desc: '判断字典项是否存在',
+});
+api.push({
+    alias: 'RacPermMenuCtrl',
+    order: '13',
+    link: '权限菜单',
+    desc: '权限菜单',
+    list: []
+})
+api[12].list.push({
+    order: '1',
+    desc: '添加权限菜单',
+});
+api[12].list.push({
+    order: '2',
+    desc: '添加/修改权限菜单',
+});
+api[12].list.push({
+    order: '3',
+    desc: '修改权限菜单的信息',
+});
+api[12].list.push({
+    order: '4',
+    desc: '删除权限菜单',
+});
+api[12].list.push({
+    order: '5',
+    desc: '获取单个权限菜单的信息',
+});
+api[12].list.push({
+    order: '6',
+    desc: '判断权限菜单是否存在',
+});
+api[12].list.push({
+    order: '7',
+    desc: '查询权限菜单的信息',
+});
+api[12].list.push({
+    order: '8',
+    desc: '查询权限菜单的信息',
+});
+api.push({
+    alias: 'RacAgentSignOutCtrl',
+    order: '14',
+    link: '退出代理登录的控制器',
+    desc: '退出代理登录的控制器',
+    list: []
+})
+api[13].list.push({
+    order: '1',
+    desc: '退出代理登录',
+});
+api.push({
+    alias: 'RacUserAccountCtrl',
+    order: '15',
+    link: '带用户信息的账户',
+    desc: '带用户信息的账户',
+    list: []
+})
+api[14].list.push({
+    order: '1',
+    desc: '分页查询带有用户信息的账户',
+});
+api[14].list.push({
+    order: '2',
+    desc: '根据ID查询有用户信息的账户',
+});
+api.push({
+    alias: 'RacAgentSignInCtrl',
+    order: '16',
+    link: '代理登录的控制器',
+    desc: '代理登录的控制器',
+    list: []
+})
+api[15].list.push({
+    order: '1',
+    desc: '代理登录',
+});
+api.push({
+    alias: 'RacSignInCtrl',
+    order: '17',
+    link: '账户登录的控制器',
+    desc: '账户登录的控制器',
+    list: []
+})
+api[16].list.push({
+    order: '1',
+    desc: '通过账户名称登录',
+});
+api.push({
+    alias: 'RacSignUpCtrl',
+    order: '18',
+    link: 'api账户注册的控制器',
+    desc: 'API账户注册的控制器',
+    list: []
+})
+api[17].list.push({
+    order: '1',
+    desc: '通过账户名称注册',
+});
+api.push({
+    alias: 'RacAccountCtrl',
     order: '19',
-    link: '权限',
-    desc: '权限',
+    link: '账户',
+    desc: '账户',
     list: []
 })
 api[18].list.push({
     order: '1',
-    desc: '添加权限',
+    desc: '添加账户',
 });
 api[18].list.push({
     order: '2',
-    desc: '修改权限的信息',
+    desc: '修改账户的信息',
 });
 api[18].list.push({
     order: '3',
-    desc: '删除权限',
+    desc: '删除账户',
 });
 api[18].list.push({
     order: '4',
-    desc: '获取单个权限的信息',
+    desc: '获取单个账户的信息',
 });
 api[18].list.push({
     order: '5',
-    desc: '判断权限是否存在',
+    desc: '判断账户是否存在',
 });
 api[18].list.push({
     order: '6',
-    desc: '查询权限的信息',
+    desc: '查询账户的信息',
 });
 api[18].list.push({
     order: '7',
-    desc: '查询带分组的权限列表',
+    desc: '修改账户登录密码',
 });
 api[18].list.push({
     order: '8',
-    desc: '上移动权限的信息',
+    desc: '启用账户',
 });
 api[18].list.push({
     order: '9',
-    desc: '下移动权限的信息',
+    desc: '禁用账户',
 });
 api[18].list.push({
     order: '10',
-    desc: '启用权限',
+    desc: '上传头像',
 });
 api[18].list.push({
     order: '11',
-    desc: '禁用权限',
+    desc: '获取当前账户信息',
+});
+api[18].list.push({
+    order: '12',
+    desc: '查询账户的信息',
 });
 api.push({
     alias: 'RacRealmCtrl',
