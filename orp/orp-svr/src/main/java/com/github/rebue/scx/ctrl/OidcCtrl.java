8dcc39fa22973385093e91d0a65d64aae3fa7a0f
lq <1213811243@qq.com>
2021-08-31 11:33:57 +0800

    oidc流程基本完成

--- orp/orp-svr/src/main/java/com/github/rebue/scx/ctrl/OidcCtrl.java
+++ orp/orp-svr/src/main/java/com/github/rebue/scx/ctrl/OidcCtrl.java
@@ -3,11 +3,14 @@ package com.github.rebue.scx.ctrl;
 import com.github.rebue.scx.svc.OidcSvc;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/oidc")
@@ -22,7 +25,9 @@ public class OidcCtrl {
     {
         Pair<String, String> pair = oidcSvc.callback(request, response, code);
         if (pair.getLeft() != null) {
-            return "redirect:" + pair.getLeft();
+            response.setStatusCode(HttpStatus.FOUND);
+            response.getHeaders().setLocation(URI.create(pair.getLeft()));
+            return null;
         }
         return pair.getRight();
     }
