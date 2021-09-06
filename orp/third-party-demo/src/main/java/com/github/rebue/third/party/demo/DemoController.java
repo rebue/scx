07953005543a819ec183efd54097f3088e9ab875
lq <luqingterity@outlook.com>
2021-09-06 22:05:11 +0800

    gateway配置

--- orp/third-party-demo/src/main/java/com/github/rebue/third/party/demo/DemoController.java
+++ orp/third-party-demo/src/main/java/com/github/rebue/third/party/demo/DemoController.java
@@ -1,14 +1,16 @@
 package com.github.rebue.third.party.demo;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
+import com.github.rebue.orp.core.OidcCore;
+import com.github.rebue.third.party.demo.utils.CookieUtil;
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.oauth2.sdk.TokenResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.github.rebue.orp.core.OidcCore;
-import com.github.rebue.third.party.demo.utils.CookieUtil;
-import com.nimbusds.oauth2.sdk.TokenResponse;
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class DemoController {
@@ -19,7 +21,7 @@ public class DemoController {
     @Resource
     private Configurations configurations;
 
-    @GetMapping(value = {"/", "/index.html"})
+    @GetMapping("/")
     public String index(HttpServletRequest request)
     {
         Boolean isLogin = CookieUtil.getFirstCookieValue(request, Configurations.LOGIN_COOKIE)
@@ -32,7 +34,7 @@ public class DemoController {
     }
 
     @GetMapping("/callback")
-    public String callback(String code) throws Exception
+    public String callback(HttpServletResponse response, String code) throws Exception
     {
         TokenResponse tokenResponse = OidcCore.tokenRequest(
                 configurations.getTokenEndpoint(),
@@ -42,6 +44,13 @@ public class DemoController {
                 configurations.getRedirect()
         );
         if (tokenResponse.indicatesSuccess()) {
+            JWT idToken = tokenResponse.toSuccessResponse().getTokens().toOIDCTokens().getIDToken();
+            String sign = jwt.sign(idToken.getJWTClaimsSet().getSubject());
+            Cookie cookie = new Cookie(Configurations.LOGIN_COOKIE, sign);
+            cookie.setDomain("the-localhost");
+            cookie.setMaxAge(-1);
+            cookie.setPath("/");
+            response.addCookie(cookie);
             return "redirect:index.html";
         }
         return "redirect:callback_error.html";
