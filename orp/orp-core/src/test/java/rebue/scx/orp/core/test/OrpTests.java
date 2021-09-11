ae8fc7eb6980b2e0fe0514328672df0486b06c9d
zbz <nnzbz@21cn.com>
2021-09-11 11:23:04 +0800

    * 完成钉钉扫码登录的单元测试

--- orp/orp-core/src/test/java/rebue/scx/orp/core/test/OrpTests.java
+++ orp/orp-core/src/test/java/rebue/scx/orp/core/test/OrpTests.java
@@ -2,13 +2,8 @@ package rebue.scx.orp.core.test;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 import rebue.scx.orp.core.cache.StateCache;
 import rebue.scx.orp.core.config.StrategyConfig;
 import rebue.scx.orp.core.dic.OrpTypeDic;
@@ -17,22 +12,23 @@ import rebue.scx.orp.core.ro.UserInfoRo;
 import rebue.scx.orp.core.strategy.Strategy;
 import rebue.scx.orp.core.to.AuthCodeTo;
 import rebue.scx.orp.core.to.AuthTo;
-import rebue.wheel.net.httpclient.HttpClient;
-import rebue.wheel.net.httpclient.impl.ApacheHttpClientImpl;
 
 import java.time.Duration;
+import java.util.Scanner;
 
-@RunWith(SpringRunner.class)
 @Slf4j
 public class OrpTests {
     private static StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
 
-    private HttpClient                 _httpClient         = new ApacheHttpClientImpl();
+    public static void main(String[] args) {
+        init();
+        test01();
+        // test02();
+    }
 
     /**
      * 初始化redis连接
      */
-    @BeforeAll
     public static void init() {
         LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory("127.0.0.1", 6379);
         connectionFactory.afterPropertiesSet();
@@ -43,12 +39,10 @@ public class OrpTests {
     /**
      * 获取钉钉认证的URL，并认证授权码
      */
-    @Test
-    @Disabled
     @SneakyThrows
-    public void test01() {
-        String         clientId     = "dingsahvlqybvb44jd50";
-        String         clientSecret = "hWt9A-ebYcR30DoXqJHLcka62jpR3ZKMc6GFizNW-5xczbahLr0yf7plEO7ewm80\n";
+    public static void test01() {
+        String         clientId     = "ding****************";
+        String         clientSecret = "********************";
         String         redirectUri  = "http://www.abc.com";
 
         StrategyConfig orpConfig    = StrategyConfig.builder().isCheckState(true).build();
@@ -57,27 +51,34 @@ public class OrpTests {
         String         authUrl      = strategy.getAuthUrl(AuthTo.builder().clientId(clientId).redirectUri(redirectUri).build());
         log.info("获取钉钉认证的URL为: {}", authUrl);
 
-        String authRoStr = _httpClient.get(authUrl);
-        log.info("认证返回: {}", authRoStr);
+        String code;
+        String state;
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("************************************************************************");
+            System.out.println("* 请点击上面的链接，用钉钉扫码后，输入链接中的code和state继续往下执行");
+            System.out.println("************************************************************************");
+            System.out.println("请输入code，并按回车: ");
+            code = s.nextLine().trim();
+            System.out.println("请输入state，并按回车: ");
+            state = s.nextLine().trim();
+        }
 
         UserInfoRo ro = strategy.authCode(AuthCodeTo.builder()
                 .clientId(clientId)
                 .clientSecret(clientSecret)
-                .state("STATE")
-                .code("CODE")
+                .code(code)
+                .state(state)
                 .build());
-        log.info("认证授权码的结果为: {}", ro);
+        log.info("钉钉认证授权码的结果为: {}", ro);
     }
 
     /**
      * 获取微信认证的URL, 并认证授权码
      */
-    @Test
-    // @Disabled
     @SneakyThrows
-    public void test02() {
-        String         clientId     = "wxcbe623e1635842d9";
-        String         clientSecret = "0d23fc6c83325585ac232d08d4635158";
+    public static void test02() {
+        String         clientId     = "wx****************";
+        String         clientSecret = "******************";
         String         redirectUri  = "http://www.abc.com";
 
         StrategyConfig orpConfig    = StrategyConfig.builder().isCheckState(true).build();
@@ -86,16 +87,25 @@ public class OrpTests {
         String         authUrl      = strategy.getAuthUrl(AuthTo.builder().clientId(clientId).redirectUri(redirectUri).build());
         log.info("获取微信认证的URL为: {}", authUrl);
 
-        String authRoStr = _httpClient.get(authUrl);
-        log.info("认证返回: {}", authRoStr);
+        String code;
+        String state;
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("************************************************************************");
+            System.out.println("* 请点击上面的链接，用钉钉扫码后，输入链接中的code和state继续往下执行");
+            System.out.println("************************************************************************");
+            System.out.println("请输入code，并按回车: ");
+            code = s.nextLine().trim();
+            System.out.println("请输入state，并按回车: ");
+            state = s.nextLine().trim();
+        }
 
         UserInfoRo ro = strategy.authCode(AuthCodeTo.builder()
                 .clientId(clientId)
                 .clientSecret(clientSecret)
-                .state("STATE")
-                .code("CODE")
+                .code(code)
+                .state(state)
                 .build());
-        log.info("认证授权码的结果为: {}", ro);
+        log.info("微信认证授权码的结果为: {}", ro);
     }
 
 }
