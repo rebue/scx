270a99c5850943407ada32762e1afd78d549bc79
lq <luqingterity@outlook.com>
2021-09-13 19:41:56 +0800

    修改包名

--- oap/oap-svr/src/main/java/rebue/scx/orp/OapApplication.java
+++ oap/oap-svr/src/main/java/rebue/scx/orp/OapApplication.java
@@ -3,16 +3,13 @@ package rebue.scx.orp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @SpringCloudApplication
 @EnableCaching
-@ComponentScans(value = { @ComponentScan(value = "rebue.scx.rac"), @ComponentScan(value = "com.github.rebue.scx")
-})
 public class OapApplication {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args)
+    {
         SpringApplication.run(OapApplication.class, args);
     }
 
