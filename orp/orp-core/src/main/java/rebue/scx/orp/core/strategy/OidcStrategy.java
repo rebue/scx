8a469c5276aec199a851b5e963a8e267e8eaade7
lq <luqingterity@outlook.com>
2021-09-29 16:14:42 +0800

    todo 加到yml

--- orp/orp-core/src/main/java/rebue/scx/orp/core/strategy/OidcStrategy.java
+++ orp/orp-core/src/main/java/rebue/scx/orp/core/strategy/OidcStrategy.java
@@ -1,5 +1,6 @@
 package rebue.scx.orp.core.strategy;
 
+import lombok.SneakyThrows;
 import rebue.scx.orp.core.cache.StateCache;
 import rebue.scx.orp.core.config.StrategyConfig;
 import rebue.scx.orp.core.dic.OrpTypeDic;
@@ -8,25 +9,37 @@ import rebue.scx.orp.core.to.AuthCodeTo;
 import rebue.scx.orp.core.to.AuthTo;
 import rebue.wheel.net.httpclient.HttpClient;
 
+import java.net.URLEncoder;
 import java.util.Map;
+import java.util.UUID;
 
 public class OidcStrategy extends AbstractStrategy<AuthCodeTo, Void, Void, AuthCodeTo, Void> {
+
+    private StateCache stateCache;
 
     public OidcStrategy(StrategyConfig orpConfig, Map<String, ClientMo> clients, StateCache stateCache, HttpClient httpClient, Map<String, String> extras)
     {
         super(orpConfig, clients, stateCache, httpClient);
+        this.stateCache = stateCache;
     }
 
     @Override
-    protected void mapperRegister()
+    protected void checkAuthTo(final AuthTo authTo)
     {
-        System.out.println();
     }
 
     @Override
     protected void fillAuthToDefaultValue(AuthTo authTo)
     {
-        System.out.println();
+    }
+
+    @Override
+    protected void mapperRegister()
+    {
+        _mapperFactory.classMap(AuthTo.class, Map.class)
+                .field("scopes", "scope")
+                .field("redirectUri", "redirect_uri")
+                .byDefault().register();
     }
 
     @Override
@@ -44,13 +57,29 @@ public class OidcStrategy extends AbstractStrategy<AuthCodeTo, Void, Void, AuthC
     @Override
     public OrpTypeDic getOrpType()
     {
-        return null;
+        return OrpTypeDic.Oidc;
     }
 
+    String callUri = "http://127.0.0.1";     // todo 加到yml
+    String redirectUri = "http://127.0.0.1"; // todo 加到yml
+    String clientId = "unified-auth";        // todo 加到yml
+
+    @SneakyThrows
     @Override
-    protected String authUrl()
+    public String getAuthUrl(final AuthTo authTo)
     {
-        return super.authUrl();
+        String state = UUID.randomUUID().toString();
+        stateCache.set(OrpTypeDic.Oidc.name(), authTo.getClientId(), state);
+        String authUrl = callUri + "/oap-svr/oap/authorize?scope=openid&response_type=code"
+                + "&client_id=" + clientId
+                + "&state=" + state
+                + "&redirect_uri=";
+        if (clientId.equals(authTo.getClientId())) {
+            authUrl += URLEncoder.encode(redirectUri + "/orp-svr/orp/callback", "utf-8");
+        } else {
+            authUrl += authTo.getRedirectUri();
+        }
+        return authUrl;
     }
 
 }
