bffc8d99eb45d46e9554b4b379f9a0ab924f7d3b
zbz <nnzbz@21cn.com>
2021-11-07 07:57:18 +0800

    * 调整libs路径

--- 文档/build.sh
+++ 文档/build.sh
@@ -16,7 +16,7 @@ cd /home/yuanman/git/common-pom/sb-dependencies/sb-parent/
 mvn clean install -DskipTests
 cd /home/yuanman/git/mbgx/
 mvn clean install -DskipTests
-cd /home/yuanman/git/scx/orp/orp-core/lib/
+cd /home/yuanman/git/scx/orp/orp-core/libs/
 mvn install:install-file -Dfile=taobao-sdk-java-auto_1479188381469-20210908.jar -DgroupId=com.dingtalk.open -DartifactId=taobao-sdk-java-auto -Dversion=20210908 -Dpackaging=jar
 cd /home/yuanman/git/scx/
 mvn clean install -DskipTests
