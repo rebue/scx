1cc29eb72a2350d0dae445ad34d286b7e0026ffe
heguozhan <2326375069@qq.com>
2021-12-31 15:42:07 +0800

    upload文件增加nacos部署模式选择

--- jwt/jwt-svr/upload.sh
+++ jwt/jwt-svr/upload.sh
@@ -8,6 +8,34 @@ if [ -n "${REMOTE_LOGIN_NAME}" ];then
  	REMOTE_LOGIN_NAME=${REMOTE_LOGIN_NAME}@
 fi
 
+echo 请选择nacos配置：
+NACOS_CONF=
+IS_TRUE=true
+while $IS_TRUE
+do
+	echo 1.单机  2.集群  3.自定义
+	read NACOS_MODLE
+	case ${NACOS_MODLE} in 
+		1) 
+		NACOS_MODLE="单机"
+		NACOS_CONF='nacos1:8848'
+		IS_TRUE=false
+		;;
+		2) 
+		NACOS_MODLE="集群"
+		IS_TRUE=false
+		;;
+		3) 
+		NACOS_MODLE="自定义"
+		read -p  "请输入nacos配置：" CU_NACOS
+		NACOS_CONF=$CU_NACOS
+		IS_TRUE=false
+		;;
+		*)
+		echo 没有该选项，请重新选择
+	esac
+done
+
 echo ===============================
 echo 参数详情
 echo 远程主机地址：$REMOTE_HOST
@@ -30,4 +58,15 @@ TEMP_DIR="src/main/resources/static"
 if [ -d "$TEMP_DIR" ];then
  	rsync -r --progress -z $TEMP_DIR ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/
 fi
+
+case ${NACOS_MODLE} in
+	'单机')
+	ssh $REMOTE_LOGIN_NAME$REMOTE_HOST "sed -i '/server-addr/s/:.*/: ${NACOS_CONF}/g' /usr/local/${SVR_NAME}/config/bootstrap-prod.yml"
+	;;
+	'集群')
+	;;
+	'自定义')
+	ssh $REMOTE_LOGIN_NAME$REMOTE_HOST "sed -i '/server-addr/s/:.*/: ${NACOS_CONF}/g' /usr/local/${SVR_NAME}/config/bootstrap-prod.yml"
+	;;
+esac
 echo ===============================
--- msg/msg-svr/upload.sh
+++ msg/msg-svr/upload.sh
@@ -8,11 +8,40 @@ if [ -n "${REMOTE_LOGIN_NAME}" ];then
  	REMOTE_LOGIN_NAME=${REMOTE_LOGIN_NAME}@
 fi
 
+echo 请选择nacos配置：
+NACOS_CONF=
+IS_TRUE=true
+while $IS_TRUE
+do
+	echo 1.单机  2.集群  3.自定义
+	read NACOS_MODLE
+	case ${NACOS_MODLE} in 
+		1) 
+		NACOS_MODLE="单机"
+		NACOS_CONF='nacos1:8848'
+		IS_TRUE=false
+		;;
+		2) 
+		NACOS_MODLE="集群"
+		IS_TRUE=false
+		;;
+		3) 
+		NACOS_MODLE="自定义"
+		read -p  "请输入nacos配置：" CU_NACOS
+		NACOS_CONF=$CU_NACOS
+		IS_TRUE=false
+		;;
+		*)
+		echo 没有该选项，请重新选择
+	esac
+done
+
 echo ===============================
 echo 参数详情
 echo 远程主机地址：$REMOTE_HOST
 echo 远程登录账户：$REMOTE_LOGIN_NAME
 echo 微服务的名称：$SVR_NAME
+echo nacos配置：$NACOS_MODLE:$NACOS_CONF
 echo ===============================
 
 rsync --progress -z target/${SVR_NAME}-*.jar ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/ --exclude='*-javadoc.jar' --exclude='*-sources.jar'
@@ -30,4 +59,15 @@ TEMP_DIR="src/main/resources/static"
 if [ -d "$TEMP_DIR" ];then
  	rsync -r --progress -z $TEMP_DIR ${REMOTE_LOGIN_NAME}${REMOTE_HOST}:/usr/local/${SVR_NAME}/
 fi
+
+case ${NACOS_MODLE} in
+	'单机')
+	ssh $REMOTE_LOGIN_NAME$REMOTE_HOST "sed -i '/server-addr/s/:.*/: ${NACOS_CONF}/g' /usr/local/${SVR_NAME}/config/bootstrap-prod.yml"
+	;;
+	'集群')
+	;;
+	'自定义')
+	ssh $REMOTE_LOGIN_NAME$REMOTE_HOST "sed -i '/server-addr/s/:.*/: ${NACOS_CONF}/g' /usr/local/${SVR_NAME}/config/bootstrap-prod.yml"
+	;;
+esac
 echo ===============================
