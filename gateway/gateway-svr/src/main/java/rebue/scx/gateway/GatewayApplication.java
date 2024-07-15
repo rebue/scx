7084d9ae92bb368844ef1fada5e5ec4837b14289
zbz <nnzbz@21cn.com>
2024-07-15 10:10:06 +0800

    * 修复代码

--- gateway/gateway-svr/src/main/java/rebue/scx/gateway/GatewayApplication.java
+++ gateway/gateway-svr/src/main/java/rebue/scx/gateway/GatewayApplication.java
@@ -8,7 +8,6 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         try {
-            System.setProperty("spring.devtools.restart.enabled", "false");
             SpringApplication.run(GatewayApplication.class, args);
         } catch (Throwable e) {
             if (!e.getClass().getName().contains("SilentExitException")) {
